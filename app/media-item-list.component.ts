32e7dd3c34ec9c57458d71f692df6faba8ab96e7
himawan-r <IMadeHimawan.Raharja@mitrais.com>
2017-03-31 16:30:01 +0800

    fix merge conflict

--- app/media-item-list.component.ts
+++ app/media-item-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, Output, EventEmitter } from '@angular/core';
-
 import { MediaItemComponent } from './media-item.component';
+import { MediaItemService } from './media-item.service';
+
 @Component({
   selector: 'mw-media-item-list',
   templateUrl: 'app/media-item-list.component.html',
@@ -8,6 +9,7 @@ import { MediaItemComponent } from './media-item.component';
 })
 export class MediaItemListComponent {
   mediaItems;
+  @Output() preview = new EventEmitter();
 
   constructor(private mediaItemService: MediaItemService) {}
 
@@ -19,73 +21,8 @@ export class MediaItemListComponent {
     this.mediaItemService.delete(mediaItem);
   }
   
-  @Output() preview = new EventEmitter();
-
-  onMediaItemDelete(mediaItem) { }
-
   onMediaItemPreview(mediaItem) {
     this.preview.emit(mediaItem);
   }
-
-  mediaItems = [
-    {
-      id: 1,
-      name: "Firebug",
-      medium: "Series",
-      category: "Science Fiction",
-      year: 2010,
-      rating: 2,
-      watchedOn: 1294166565384,
-      isFavorite: false,
-      posters: [
-        {imgSrc: './media/firebug1.png', selected: true, isAvailableFullSize : true},
-        {imgSrc: './media/firebug2.png', selected: false, isAvailableFullSize : false}
-      ]
-    },
-    {
-      id: 2,
-      name: "The Small Tall",
-      medium: "Movies",
-      category: "Comedy",
-      year: 2015,
-      rating: 3.5,
-      watchedOn: null,
-      isFavorite: true,
-      posters: [
-        {imgSrc: './media/smalltall1.png', selected: true, isAvailableFullSize : true},
-        {imgSrc: './media/smalltall2.png', selected: false, isAvailableFullSize : true}
-      ]
-    }, {
-      id: 3,
-      name: "The Redemption",
-      medium: "Movies",
-      category: "Action",
-      year: 2016,
-      rating: 4.7,
-      watchedOn: null,
-      isFavorite: false,
-      imgSrc : null
-    }, {
-      id: 4,
-      name: "Hoopers",
-      medium: "Series",
-      category: "Drama",
-      year: null,
-      rating: 3.2,
-      watchedOn: null,
-      isFavorite: true,
-      imgSrc : null
-    }, {
-      id: 5,
-      name: "Happy Joe: Cheery Road",
-      medium: "Movies",
-      category: "Action",
-      year: 2015,
-      rating: 2.7,
-      watchedOn: 1457166565384,
-      isFavorite: false,
-      imgSrc : null
-    }
-  ];
 
 }
