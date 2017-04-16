6ecc78e9ce25a0369af79f77842eae6aa59f06f9
himawan-r <IMadeHimawan.Raharja@mitrais.com>
2017-04-16 19:24:07 +0800

    add popup

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { YoloNavigationComponent } from './navigation/yolo-navigation.component';
 import { YoloItemComponent } from './item/yolo-item.component';
@@ -9,8 +10,10 @@ import { YoloItemFormComponent } from './item/yolo-item-form.component';
 import { YoloRegistrationComponent } from './registration/yolo-registration.component';
 import { YoloLoginComponent } from './login/yolo-login.component';
 import { YoloPopupComponent } from './popup/yolo-popup.component';
+
 import { AuthenticationService } from './service/auth.service';
 import { PopupService } from './service/common-popup.service';
+
 import { lookupListToken, lookupLists} from './common/constant';
 import { routing } from './app.routing';
 
